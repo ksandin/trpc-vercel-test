e694ddc5fbff632e858ac01cdae5571b7fe4323b
ksandin <kasper.sandin@gmail.com>
2022-11-24 21:21:25 +0100

    Refactor

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { createApiClients } from "./apiClients";
 import { trpc } from "./trpc";
-import { QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
 
-const {trpcClient, queryClient} = createApiClients();
+const queryClient = new QueryClient();
+const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/trpc',
+    }),
+  ],
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
