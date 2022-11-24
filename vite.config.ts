73ff4fe4708e201333ce516bdeef2d1af8f412c2
ksandin <kasper.sandin@gmail.com>
2022-11-24 23:42:57 +0100

    tsc should use src/app as project

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   publicDir: path.resolve(__dirname, "src/app/public"),
   build: {
     outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true
   },
   plugins: [react()],
   envDir: __dirname,
