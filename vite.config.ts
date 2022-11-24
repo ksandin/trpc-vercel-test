508b555bc8dfa25bd977f1e565119d098b7361d7
ksandin <kasper.sandin@gmail.com>
2022-11-24 21:16:34 +0100

    Move client code into src/app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: path.resolve("src/app"),
   plugins: [react()]
 })
