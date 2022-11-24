f0440caa15737605f1570003c03b1fd696db7ada
ksandin <kasper.sandin@gmail.com>
2022-11-24 22:20:21 +0100

    Correct public dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.resolve(__dirname, "src/app"),
+  publicDir: path.resolve(__dirname, "src/app/public"),
   build: {
     outDir: path.resolve(__dirname, "dist"),
   },
